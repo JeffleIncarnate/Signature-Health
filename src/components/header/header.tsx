008a7555ac9ad26da47324e3d668d7e5cadab49a
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-05-03 21:44:00 +1200

    feat: minor changes to font, and header to make it look better

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,11 +7,9 @@ const Header = () => {
     <header className="Header">
       <Navbar />
       <div className="Header__Wrapper">
-        <h1>Signature</h1>
-        <h2>Health</h2>
+        <h1>SIGNATURE</h1>
+        <h2>HEALTH</h2>
       </div>
-
-      <div className="Header__Circle"></div>
     </header>
   );
 };
