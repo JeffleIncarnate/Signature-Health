1cb65e077afb06a171e723b21c09aaf81124f760
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-05-15 20:08:16 +1200

    feat: about page mostly done

--- src/components/aboutMain/aboutMain.tsx
+++ src/components/aboutMain/aboutMain.tsx
@@ -1,5 +1,12 @@
 import "./aboutMain.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faHandshake,
+  faAward,
+} from "@fortawesome/free-solid-svg-icons";
+
 const AboutMain = () => {
   return (
     <main className="AboutMain">
@@ -18,6 +25,68 @@ const AboutMain = () => {
         <div className="AboutUs__Image">
           <img src="/pictures/aboutImage.jpeg" alt="" />
         </div>
+      </div>
+
+      <div className="Values">
+        <div className="Value">
+          <FontAwesomeIcon className="Value__Icon" icon={faHeart} />
+          <h3>Compassion</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+            libero!
+          </p>
+        </div>
+
+        <div className="Value">
+          <FontAwesomeIcon className="Value__Icon" icon={faHandshake} />
+          <h3>Trust</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+            libero!
+          </p>
+        </div>
+
+        <div className="Value">
+          <FontAwesomeIcon className="Value__Icon" icon={faAward} />
+          <h3>Excellence</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+            libero!
+          </p>
+        </div>
+      </div>
+
+      <div className="Philosophy">
+        <h2>Philosophy</h2>
+
+        <p>“Genuine care for all stakeholders”</p>
+      </div>
+
+      <div className="Mission">
+        <div className="MissionWrapper">
+          <h2>Mission</h2>
+
+          <p>
+            At Signature health, our mission is to enable our clients to
+            continue living enriched lives within their communities, in comforts
+            of their home and surrounded by their whanau.
+          </p>
+        </div>
+      </div>
+
+      <div className="Vision__Commitment">
+        <p>
+          Our vision is to become largest home care and community support
+          service in New Zealand.
+        </p>
+        <p>
+          At Signature Health, we are committed to ensuring that every
+          individual we serve receives dignified, culturally appropriate care
+          that honours their unique identity and respects their inherent rights.
+          We stand as allies in the pursuit of holistic well- being for all,
+          fostering a compassionate environment where the spirit of the Treaty
+          flourishes, and every individual’s tikanaga is valued.
+        </p>
       </div>
     </main>
   );
