581ee4c9379125e4296b92e3762cca1bf1db76b4
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-06-09 18:35:42 +1200

    feat: navbar open done

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,14 +4,15 @@ import Navbar from "../navbar/navbar.tsx";
 
 const Header = () => {
   return (
-    <header className="Header">
-      <Navbar />
-
-      <div className="OurTeam">
-        <a href="/ourTeam">Our Team</a>
-        <a href="/joinOurTeam">Join Our Team</a>
-      </div>
-    </header>
+    <>
+      <header className="Header">
+        <Navbar />
+        <div className="OurTeam">
+          <a href="/ourTeam">Our Team</a>
+          <a href="/joinOurTeam">Join Our Team</a>
+        </div>
+      </header>
+    </>
   );
 };
 
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,24 +1,57 @@
 import "./navbar.scss";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
-  return (
-    <nav className="Navbar">
-      <div className="Navbar__Home">
-        <a href="/">Home</a>
-      </div>
+  const [navOpen, setNavOpen] = useState<boolean>(false);
 
-      <div className="Navbar__Links">
+  useEffect(() => {
+    console.log(navOpen);
+  }, [navOpen]);
+
+  return (
+    <>
+      <nav className="Navbar">
+        <div className="Navbar__Home">
+          <a href="/">Home</a>
+        </div>
+
+        <div className="Navbar__Links">
+          <a href="/about">About Us</a>
+          <a href="/services">Our Services</a>
+          <a href="/contact">Contact Us</a>
+        </div>
+
+        <div className="Navbar__Small">
+          <FontAwesomeIcon
+            onClick={() => {
+              let entireNav = document.getElementById("Navbar__Side");
+
+              entireNav!.style.width = "100lvw";
+            }}
+            className="Navbar__Small__Icon"
+            icon={faBars}
+          />
+        </div>
+      </nav>
+
+      <div id="Navbar__Side">
+        <FontAwesomeIcon
+          onClick={() => {
+            let entireNav = document.getElementById("Navbar__Side");
+
+            entireNav!.style.width = "0";
+          }}
+          className="Navbar__Side__Icon"
+          icon={faXmark}
+        />
         <a href="/about">About Us</a>
         <a href="/services">Our Services</a>
         <a href="/contact">Contact Us</a>
       </div>
-
-      <div className="Navbar__Small">
-        <FontAwesomeIcon className="Navbar__Small__Icon" icon={faBars} />
-      </div>
-    </nav>
+    </>
   );
 };
 
