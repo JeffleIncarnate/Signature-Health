6ff2e37f0c90b2270d577f2e0d6f16812b428a32
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-06-09 14:11:20 +1200

    feat: header and navbar responsive

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,24 +1,22 @@
 import "./navbar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav className="Navbar">
       <div className="Navbar__Home">
-        <div>
-          <a href="/">Home</a>
-        </div>
+        <a href="/">Home</a>
       </div>
 
       <div className="Navbar__Links">
-        <div>
-          <a href="/about">About Us</a>
-        </div>
-        <div>
-          <a href="/services">Our Services</a>
-        </div>
-        <div>
-          <a href="/contact">Contact Us</a>
-        </div>
+        <a href="/about">About Us</a>
+        <a href="/services">Our Services</a>
+        <a href="/contact">Contact Us</a>
+      </div>
+
+      <div className="Navbar__Small">
+        <FontAwesomeIcon className="Navbar__Small__Icon" icon={faBars} />
       </div>
     </nav>
   );
