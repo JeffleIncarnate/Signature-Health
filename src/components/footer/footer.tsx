1660ab2a31792ede18af199fd77f3904e4f91d81
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-06-14 20:11:49 +1200

    feat: new footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,26 +6,42 @@ import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="Footer">
-      <div className="Name">
-        <h2>Signature Health</h2>
-      </div>
+    // <footer className="Footer">
+    //   <div className="Name">
+    //     <h2>Signature Health</h2>
+    //   </div>
 
-      <div className="SectionRight">
-        <div className="Links">
-          <a href="/about">About Us</a>
-          <a href="/services">Our Services</a>
-          <a href="/contact">Contact Us</a>
-        </div>
-        <div className="Chat">
-          <p>Contact Us</p>
-          <div className="Chat__Socials">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <FontAwesomeIcon icon={faPhone} />
-            <FontAwesomeIcon icon={faLinkedin} />
-            <FontAwesomeIcon icon={faFacebook} />
-          </div>
-        </div>
+    //   <div className="SectionRight">
+    //     <div className="Links">
+    //       <a href="/about">About Us</a>
+    //       <a href="/services">Our Services</a>
+    //       <a href="/contact">Contact Us</a>
+    //     </div>
+    //     <div className="Chat">
+    //       <p>Contact Us</p>
+    //       <div className="Chat__Socials">
+    //         <FontAwesomeIcon icon={faEnvelope} />
+    //         <FontAwesomeIcon icon={faPhone} />
+    //         <FontAwesomeIcon icon={faLinkedin} />
+    //         <FontAwesomeIcon icon={faFacebook} />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </footer>
+
+    <footer className="Footer">
+      <div className="Links">
+        <a href="/about">About Us</a>
+        <i>•</i>
+        <a href="/services">Our Services</a>
+        <i>•</i>
+        <a href="/contact">Contact Us</a>
+      </div>
+      <div className="Socials">
+        <FontAwesomeIcon icon={faEnvelope} />
+        <FontAwesomeIcon icon={faPhone} />
+        <FontAwesomeIcon icon={faLinkedin} />
+        <FontAwesomeIcon icon={faFacebook} />
       </div>
     </footer>
   );
