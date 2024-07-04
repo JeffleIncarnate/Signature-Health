b1f44b215c17026b7741b80821c9f3421e953c38
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-07-04 20:24:10 +1200

    feat: made a bunch of changes and forgor :skull:

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,29 +6,6 @@ import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    // <footer className="Footer">
-    //   <div className="Name">
-    //     <h2>Signature Health</h2>
-    //   </div>
-
-    //   <div className="SectionRight">
-    //     <div className="Links">
-    //       <a href="/about">About Us</a>
-    //       <a href="/services">Our Services</a>
-    //       <a href="/contact">Contact Us</a>
-    //     </div>
-    //     <div className="Chat">
-    //       <p>Contact Us</p>
-    //       <div className="Chat__Socials">
-    //         <FontAwesomeIcon icon={faEnvelope} />
-    //         <FontAwesomeIcon icon={faPhone} />
-    //         <FontAwesomeIcon icon={faLinkedin} />
-    //         <FontAwesomeIcon icon={faFacebook} />
-    //       </div>
-    //     </div>
-    //   </div>
-    // </footer>
-
     <footer className="Footer">
       <div className="Links">
         <a href="/about">About Us</a>
